9af82a85255898314105039d92548a750e94e01c
brightkut <dsorn2@gmail.com>
2024-08-22 09:50:37 +0700

    update directory

--- BE/src/main/java/com/brightkut/aroundtheworld/AroundTheWorldApplication.java
+++ BE/src/main/java/com/brightkut/aroundtheworld/AroundTheWorldApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class
 AroundTheWorldApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(AroundTheWorldApplication.class, args);
     }
